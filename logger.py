f119f8869e8573f0c34f49fa6f29e17d1089c926
Ike <ikescode@gmail.com>
2019-10-08 13:57:33 -0700

    Revert "log infection survival"
    This reverts commit bfaa8df60afd84b112765d6c182acf6f5cce68e2.

--- logger.py
+++ logger.py
@@ -51,14 +51,14 @@ class Logger(object):
         # exactly what happened in the interaction and create a String, and write to your logfile.
         log_file = open(self.file_name, "a+")
         if did_infect:
-            log_file.write(f"{person._id} infects {random_person._id}\n")
+            log_file.write(f"{person._id} infects {random_person._id}")
         else:
             reason = ""
             if person.is_vaccinated:
                 reason = 'they are vaccinated.'
             else:
                 reason = 'they are already sick.'
-            log_file.write(f"{person._id} didn't infect {random_person._id} because {reason}\n")
+            log_file.write(f"{person._id} didn't infect {random_person._id} because {reason}")
         log_file.close()
 
     def log_infection_survival(self, person, did_die_from_infection):
@@ -71,11 +71,6 @@ class Logger(object):
         # TODO: Finish this method. If the person survives, did_die_from_infection
         # should be False.  Otherwise, did_die_from_infection should be True.
         # Append the results of the infection to the logfile
-        log_file = open(self.file_name, "a+")
-        if person.did_survive_infection():
-            log_file.write(f'{person._id} died from infection\n')
-        elif not person.did_survive_infection() or not did_die_from_infection:
-            log_file.write(f'{person._id} survived the infection\n')
         pass
 
     def log_time_step(self, time_step_number):
@@ -97,3 +92,9 @@ class Logger(object):
         # new one begins.
         # NOTE: Here is an opportunity for a stretch challenge!
         pass
+
+
+log = Logger('log.txt')
+person = Person(1, True, Virus("HIV", 0.8, 0.3))
+random_person = Person(2, True, Virus("HIV", 0.8, 0.3))
+log.log_interaction(person, random_person)
